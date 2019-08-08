6199d4017a260b719953a83b351da80962f0626b
Fabricio Catae <fcatae@hotmail.com>
2019-08-07 21:30:17 -0300

    initial commit

---
+++ oraculo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oraculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
